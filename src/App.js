670b7f25c636a3add4f1b21981ca08c904fa57a5
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-04-05 01:03:00 +0300

    Add page switching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import './App.css';
+import Content from './Components/Content';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+
+
+  const navigate = useNavigate();
   return (
-    <div className="content">
-      
+    <div id='main-wrapper'>
+      <Header navigate={navigate}/>
+      <Content />
+      <Footer />
     </div>
   );
 }
