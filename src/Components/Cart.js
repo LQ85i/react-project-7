ab89a14d73a5c29b62b7dffe1f36d3c28734fb88
Funxy <jaakko.kakko97@gmail.com>
2023-04-23 04:41:58 +0300

    Add input fields to cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -4,13 +4,15 @@ import itemData from '../Components/ItemData';
 
 function Cart() {
     const [items, setItems] = useState([])
-    
+    const maxItemCount = 99;
     
     const addItem = (itemID) => {
-        const newItems = [...items];
-        const newItem = itemData.find(obj => obj.id === itemID);
-        newItems.push(newItem);
-        setItems(newItems);
+        if(itemCount(itemID)<maxItemCount){
+            const newItems = [...items];
+            const newItem = itemData.find(obj => obj.id === itemID);
+            newItems.push(newItem);
+            setItems(newItems);
+        }
     }
 
     const removeItem = (itemID) => {
@@ -22,11 +24,40 @@ function Cart() {
         setItems(newItems);
     }
 
+    const itemCount = (itemID) => {
+        let count = 0;
+        for (let i = 0; i < items.length; i++) {
+            if(items[i].id === itemID){
+                count++;
+            }
+        }
+        return count;
+    }
+
     const clearItems = () => {
         setItems([]);
     }
 
-    return {items, addItem, removeItem, clearItems};
+    const setItemCount = (itemID, count) => {
+        const newItems = [...items];
+
+        //remove all items with itemID
+        let index = newItems.length;
+        while (index--) {
+            if(newItems[index].id === itemID){
+                newItems.splice(index,1);
+            }
+        }
+        //add count amount of itemID's
+        
+        for (let i = 0; i < count; i++) {
+            const newItem = itemData.find(obj => obj.id === itemID);
+            newItems.push(newItem);
+        }
+        setItems(newItems);
+    }
+
+    return {items, addItem, removeItem, clearItems, setItemCount};
 }
 
 export default Cart
