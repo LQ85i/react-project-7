ae84e2bb4e128a976fa64c8506c9941b898e09f0
Funxy <jaakko.kakko97@gmail.com>
2023-05-07 18:46:28 +0300

    Add content and styles to home page and header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,11 @@
 import shopping_cart from "../Images/icon_shopping_cart.svg"
+import burger_logo from "../Images/burger_logo.png";
 
 const Header = (props) => {
     const { navigate, cart, showCart, setShowCart } = props;
 
     const handleClick = (e) => {
-        if (e.currentTarget.className === "home") {
-            navigate("/");
-        } else if (e.currentTarget.className === "store") {
+        if (e.currentTarget.className === "store") {
             navigate("/store");
         } else if (e.currentTarget.className === "icon-cart") {
             if(showCart === "hidden"){
@@ -15,15 +14,23 @@ const Header = (props) => {
                 setShowCart("hidden")
             }
             
+        } else if (e.currentTarget.className === "center") {
+            navigate("/");
         }
     }
     return (
         <div id="header">
             <div className="left">
-                <button className="home" onClick={handleClick}>Home</button>
-                <button className="store" onClick={handleClick}>Store</button>
+                <button className="store" onClick={handleClick}>Products</button>
+                <button className="contact" onClick={handleClick}>Contact</button>
+                <button className="about" onClick={handleClick}>About Us</button>
             </div>
-            <div className="center"></div>
+            <div className="center" onClick={handleClick}>
+                <div>
+                    <img className="burger-logo" src={burger_logo} alt="" />
+                    <div className="logo-text">FAST FOOD</div>
+                </div>
+            </div>
             <div id="toggle-cart" className="right">
                 <div className="cart-count">{cart.getAllItemsCount()}</div>
                 <img className="icon-cart" src={shopping_cart} alt="" onClick={handleClick}/>
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,7 +1,68 @@
 import React from "react";
+import burger_stock from "../Images/burger_stock.jpg"
+import icon_placeholder from "../Images/icon_placeholder.svg"
 
-const HomePage = () => {
-    return <div>Home Page</div>
+const HomePage = (props) => {
+
+    const { navigate } = props;
+
+    const handleClick = (e) => {
+        if (e.currentTarget.className === "button-store") {
+            navigate("/store");
+        }
+    }
+
+    const cycleImages = () => {
+        return burger_stock
+    }
+
+    return (
+        <div className="container-home">
+            <div className="presentation">
+                <div className="text-background">
+                    <div className="text">Filler text Filler text<br /><br />Filler text Filler text</div>
+                    <button className="button-store" onClick={handleClick}>Order Now!</button>
+                </div>
+                <div className="image" style={{ backgroundImage: 'url(' + cycleImages() + ')' }} />
+            </div>
+            <div className="info-cards">
+                <div className="card">
+                    <img className="image" src={icon_placeholder} alt="" />
+                    <div className="text">feature information</div>
+                </div>
+                <div className="card">
+                    <img className="image" src={icon_placeholder} alt="" />
+                    <div className="text">feature information</div>
+                </div>
+                <div className="card">
+                    <img className="image" src={icon_placeholder} alt="" />
+                    <div className="text">feature information</div>
+                </div>
+            </div>
+            <div className="opening-hours">
+                <div className="title">OPEN:</div>
+                <div className="hours-table">
+                    <div className="row">
+                        <div className="days">Mon - Thu: </div>
+                        <div className="hours">10 - 22</div>
+                    </div>
+                    <div className="row">
+                        <div className="days">Fri: </div>
+                        <div className="hours">10 - 23</div>
+                    </div>
+                    <div className="row">
+                        <div className="days">Sat: </div>
+                        <div className="hours">11 - 23</div>
+                    </div>
+                    <div className="row">
+                        <div className="days">Sun: </div>
+                        <div className="hours">11 - 22</div>
+                    </div>
+                </div>
+                <div className="address">Address 123, 00000 City</div>
+                <div className="phone-number">+012 0123 01230<br/>+012 0123 01231</div>
+            </div>
+        </div>)
 }
 
 export default HomePage
