5b0629128d4891245ac2cdf6be9204c4df471e57
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-05-11 00:09:17 +0300

    Add cart scrolling and some styling

--- src/Components/ItemData.js
+++ src/Components/ItemData.js
@@ -1,29 +1,53 @@
-import placeholder_img from "../Images/icon_placeholder.svg";
+import burger_stock from "../Images/burger_stock.jpg"
+import pizza_stock from "../Images/pizza_stock.png"
+import kebab_stock from "../Images/kebab_stock.png"
+import soda_stock from "../Images/soda_stock.png"
+import smoothie_stock from "../Images/smoothie_stock.jpg"
+import coffee_stock from "../Images/coffee_stock.jpg"
+import icecream_stock from "../Images/icecream_stock.jpg"
 import uniqid from 'uniqid';
 
 const itemData = (function() {
     const data = [{
-        img: placeholder_img,
-        title: "item1",
-        price: 1.50,
+        img: burger_stock,
+        title: "Burger",
+        price: 10.50,
         id: uniqid()
     },
     {
-        img: placeholder_img,
-        title: "item2",
+        img: pizza_stock,
+        title: "Pizza",
+        price: 12.00,
+        id: uniqid()
+    },
+    {
+        img: kebab_stock,
+        title: "Kebab",
+        price: 13.00,
+        id: uniqid()
+    },
+    {
+        img: soda_stock,
+        title: "Soda",
+        price: 1.00,
+        id: uniqid()
+    },
+    {
+        img: smoothie_stock,
+        title: "Smoothie",
         price: 2.00,
         id: uniqid()
     },
     {
-        img: placeholder_img,
-        title: "item3",
-        price: 3.00,
+        img: coffee_stock,
+        title: "Coffee",
+        price: 1.00,
         id: uniqid()
     },
     {
-        img: placeholder_img,
-        title: "Item With Long Name",
-        price: 40.00,
+        img: icecream_stock,
+        title: "Ice Cream",
+        price: 2.00,
         id: uniqid()
     }]
 
