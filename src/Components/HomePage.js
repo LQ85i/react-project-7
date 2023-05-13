fc58b04930e9f19c4529c73978ee7469ca7e06dc
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-05-13 05:07:04 +0300

    Add mobile navigation to info-cards and more styling

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import burger_stock from "../Images/burger_stock.jpg"
 import icon_placeholder from "../Images/icon_placeholder.svg"
+import icon_arrows_left from "../Images/icon_arrows_left.svg"
+import icon_arrows_right from "../Images/icon_arrows_right.svg"
 
 const HomePage = (props) => {
 
     const { navigate } = props;
 
+    const infoCards = useRef(0);
+
     const handleClick = (e) => {
         if (e.currentTarget.className === "button-store") {
             navigate("/store");
+        } else if(e.currentTarget.className==="arrows") {
+            if(e.currentTarget.childNodes[0].className==="right"){
+                infoCards.current.scrollTo({
+                    top: 0,
+                    left: infoCards.current.scrollLeft + window.innerWidth,
+                    behavior: 'smooth'
+                });
+            } else if(e.currentTarget.childNodes[0].className==="left"){
+                infoCards.current.scrollTo({
+                    top: 0,
+                    left: infoCards.current.scrollLeft - window.innerWidth,
+                    behavior: 'smooth'
+                });
+            }
         }
     }
 
@@ -19,30 +37,45 @@ const HomePage = (props) => {
     return (
         <div id="container-home">
             <div className="presentation">
-                <div className="text-background">
+                <div className="slide-background">
                     <div className="text">Filler text<br /><br />Filler text Filler text</div>
                     <button className="button-store" onClick={handleClick}>Order Now!</button>
                 </div>
                 <div className="image" style={{ backgroundImage: 'url(' + cycleImages() + ')' }} />
             </div>
-            <div className="info-cards">
+            <div className="info-cards" ref={infoCards}>
                 <section>
+                    <div className="arrows"></div>
                     <div className="card">
                         <img className="image" src={icon_placeholder} alt="" />
                         <div className="text">feature information</div>
                     </div>
+                    <div className="arrows" onClick={handleClick}>
+                        <img className="right" src={icon_arrows_right} alt="" />
+                    </div>
+
                 </section>
                 <section>
+                    <div className="arrows" onClick={handleClick}>
+                        <img className="left" src={icon_arrows_left} alt="" />
+                    </div>
                     <div className="card">
                         <img className="image" src={icon_placeholder} alt="" />
                         <div className="text">feature information feature information </div>
                     </div>
+                    <div className="arrows" onClick={handleClick}>
+                        <img className="right" src={icon_arrows_right} alt="" />
+                    </div>
                 </section>
                 <section>
+                    <div className="arrows" onClick={handleClick}>
+                        <img className="left" src={icon_arrows_left} alt="" />
+                    </div>
                     <div className="card">
                         <img className="image" src={icon_placeholder} alt="" />
                         <div className="text">feature information feature information feature information </div>
                     </div>
+                    <div className="arrows"></div>
                 </section>
 
             </div>
