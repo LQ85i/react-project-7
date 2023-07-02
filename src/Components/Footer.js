6b15246f4eba1e409f30d0542fcda18303495ee3
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-07-02 04:18:09 +0300

    Update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,9 @@ import React from "react";
 import '../Styles/Footer.css';
 
 const Footer = () => {
-    return <div id="footer"></div>
+    return <div id="footer">
+        <div className='left'>Website by <a href="https://github.com/LQ85i/react-project-7">LQ85i</a></div>
+    </div>
 }
 
 export default Footer
